c7f3dff01d7bd63bbdae43610cedaec9109b9b7f
nikolozianeli <nikolozianeli@gmail.com>
2021-10-14 11:27:23 +0400

    getting started

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+    import java.util.Scanner;
+    import java.util.Vector;
+
+    public class Main {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String a=sc.nextLine();
+            int count=0;
+           
+        }
+    }
